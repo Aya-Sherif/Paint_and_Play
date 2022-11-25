9f158e21fa80e0cd993487f55b402f37a882d44d
MohamedHosamM <119050930+MohamedHosamM@users.noreply.github.com>
2022-11-25 20:02:47 +0200

    Add files via upload

---
+++ save.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include <fstream>
+save::save(/*the application system*/, /*an integer count*/)
+{
+	FileName = "DRAW";
+	/*count = the integer count */;
+}
+
+void save::ReadParameters()
+{
+	/*should take the name of the file and the location to save it there*/
+
+	/*also an error massage with while loop if entered a non valid name or location */
+
+
+}
+
+void save::saveplay()
+{
+	//Write the Current Draw Color 
+	// Current Fill Color and  write the number of figures 
+	// Start Saving each figure proccess 
+	//massage appear when done
+}
+
+
+void save::Execute()
+{
+
+	//Execute the action
+
+}
